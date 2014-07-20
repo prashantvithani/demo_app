57823601f2f6506c19485f0bb318c6fa4d884724
prashantvithani <prashantvithani@gmail.com>
2014-07-20 12:11:14 +0530

    Finish user edit, update, index and destroy actions.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '4.0.8'
 gem 'bootstrap-sass'
 gem 'bootstrap_form'
 gem 'sprockets', '2.11.0'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 # Use sqlite3 as the database for Active Record
 gem 'pg'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Prashant Vithani"
-		email "prashantvithani@gmail.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "prashant@11"
 		password_confirmation "prashant@11"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
